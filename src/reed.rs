828fb441f09b83a1b4f806768e1b8148988de11a
Bob Mannino <manninobobby@icloud.com>
2025-08-25 16:46:33 +0100

    add reed switch struct

---
+++ src/reed.rs
@@ -0,0 +1,26 @@
+use esp_idf_svc::hal::gpio::{Input, InputPin, OutputPin, Pin, PinDriver, Pull};
+use esp_idf_sys::EspError;
+
+pub struct Reed<'a, P>
+where
+    P: OutputPin + InputPin,
+{
+    pin: PinDriver<'a, P, Input>,
+}
+
+impl<'a, P> Reed<'a, P>
+where
+    P: OutputPin + InputPin,
+{
+    pub fn new(pin: P) -> Result<Self, EspError> {
+        let mut pin = PinDriver::input(pin)?;
+
+        pin.set_pull(Pull::Up)?;
+
+        Ok(Self { pin })
+    }
+
+    pub fn is_on(&self) -> bool {
+        self.pin.is_high()
+    }
+}
